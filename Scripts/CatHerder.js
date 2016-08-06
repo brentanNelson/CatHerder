6ea63bf3d24c39dbc9ee5dcd28eb74e222228747
brentanNelson <brentan.nelson@hotmail.com>
2016-08-06 18:38:15 +1000

    cat runs from clicks

--- Scripts/CatHerder.js
+++ Scripts/CatHerder.js
@@ -1,6 +1,9 @@
 ﻿var cat;
+var objective;
+var catSize = 30;
 var catSpeed = 1;
 var maxSpeed = 20;
+var runSpeed = 1;
 var speedIncreaseCounter = 0;
 var speedIncreaseInterval = 2000;
 var changeDirectionInterval = 20;
@@ -13,6 +16,22 @@ var gameArea = {
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
+        window.addEventListener('mousedown', function (e) {
+            gameArea.x = e.pageX;
+            gameArea.y = e.pageY;
+        })
+        window.addEventListener('mouseup', function (e) {
+            gameArea.x = false;
+            gameArea.y = false;
+        })
+        window.addEventListener('touchstart', function (e) {
+            gameArea.x = e.pageX;
+            gameArea.y = e.pageY;
+        })
+        window.addEventListener('touchend', function (e) {
+            gameArea.x = false;
+            gameArea.y = false;
+        })
     },
     clear: function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -22,10 +41,12 @@ var gameArea = {
 
 function startGame() {
     gameArea.start();
-    cat = buildCat(30, 30, "blue", 10, 120);
+    cat = new buildCat(catSize, catSize, "blue", 10, 120);
+   // objective = new buildObjective(10, 10, "red");
 }
 
-function buildCat(width, height, color, x, y) {
+//BUILD CAT
+function buildCat(width, height, colour, x, y) {
     this.width = width;
     this.height = height;
     this.xDirection = 1;
@@ -48,7 +69,7 @@ function buildCat(width, height, color, x, y) {
         }
         else
         {
-            if (changeXDirectionCounter % changeDirectionInterval == 0) {
+            if (this.changeXDirectionCounter % changeDirectionInterval == 0) {
                 this.xDirection = randomBetween(-1, 1);
                 this.changeXDirectionCounter = randomBetween(0, changeDirectionInterval - 1);
             }
@@ -60,7 +81,7 @@ function buildCat(width, height, color, x, y) {
         }
         else
         {
-            if (changeYDirectionCounter % changeDirectionInterval == 0) {
+            if (this.changeYDirectionCounter % changeDirectionInterval == 0) {
                 this.yDirection = randomBetween(-1, 1);
                 this.changeYDirectionCounter = randomBetween(0, changeDirectionInterval - 1);
             }
@@ -68,17 +89,74 @@ function buildCat(width, height, color, x, y) {
 
         this.x += catSpeed * this.xDirection;
         this.y += catSpeed * this.yDirection;
-        context.fillStyle = color;
+        context.fillStyle = colour;
         context.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.clicked = function() {
+        var myleft = this.x - 5;
+        var myright = this.x + (this.width) + 10;
+        var mytop = this.y - 5;
+        var mybottom = this.y + (this.height) + 10; //strange buffer for bottom distance?
+        var clicked = true;
+        if ((mybottom < gameArea.y) || (mytop > gameArea.y)
+         || (myright < gameArea.x) || (myleft > gameArea.x)) {
+            clicked = false;
+        }
+        return clicked;
+    }
+    this.clickReaction = function(clickX, clickY) {
+        this.centerX = this.x + (catSize / 2);
+        this.centerY = this.y + (catSize / 2);
+
+        if (clickX > this.centerX) {
+            this.xDirection = -1;
+            this.x -= runSpeed;
+            this.changeXDirectionCounter = 0;
+        }
+        else {
+            this.xDirection = 1;
+            this.x += runSpeed;
+            this.changeXDirectionCounter = 0;
+        }
+        if (clickY > this.centerY) {
+            this.yDirection = -1;
+            this.y -= runSpeed;
+            this.changeYDirectionCounter = 0;
+        }
+        else {
+            this.yDirection = 1;
+            this.y += runSpeed;
+            this.changeYDirectionCounter = 0;
+        }
     }
     return this;
 }
 
-function updateGameArea() {
-    gameArea.clear();
-    cat.update();
-    speedIncreaseCounter++;
+//function buildObjective(width, height, colour)
+//{
+//    this.width = width;
+//    this.height = height;
+//    this.x = randomBetween(1, gameArea.x);
+//    this.y = randomBetween(1, gameArea.y);
+//    this.update = function() {
+//        var context = gameArea.context;
+//        context.fillStyle = colour;
+//        context.fillRect(this.x, this.y, this.width, this.height);
+//}
 
+function updateGameArea() 
+{
+    gameArea.clear();
+    if (gameArea.x && gameArea.y) 
+    {
+        if (cat.clicked()) 
+        {
+            cat.clickReaction(gameArea.x, gameArea.y);
+        }
+    }
+    cat.update();
+
+    speedIncreaseCounter++;
     if (speedIncreaseCounter == speedIncreaseInterval && catSpeed < maxSpeed)
     {
         catSpeed++;
